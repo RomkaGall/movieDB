2f6642a54dee9d8f56cc91a76c11660e7b85bc9d
Roman Galaniuk <romangalanuk@gmail.com>
2020-02-19 14:08:29 +0200

    Updates

---
+++ precache-manifest.ff1e31ac62900c62cc1a11f0bdb3ebb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bb6ebd66fcaf0a36fff0783781d4f2",
+    "url": "/movieDB/index.html"
+  },
+  {
+    "revision": "b30f1c4f31c8b61b07fd",
+    "url": "/movieDB/static/css/main.ad9f62a0.chunk.css"
+  },
+  {
+    "revision": "3cf44ff5daa168bb1aa6",
+    "url": "/movieDB/static/js/2.d6fe91d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movieDB/static/js/2.d6fe91d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b30f1c4f31c8b61b07fd",
+    "url": "/movieDB/static/js/main.ff9e5f7c.chunk.js"
+  },
+  {
+    "revision": "e610ef00d91eaa329f48",
+    "url": "/movieDB/static/js/runtime-main.658cfbb8.js"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/movieDB/static/media/logo.02a9430b.svg"
+  }
+]);
